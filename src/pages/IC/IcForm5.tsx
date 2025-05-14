6b2bbbee7d509ec12e21ccd1a34630e703a03db0
c3146092091 <c3146092091@gmail.com>
2025-05-14 19:05:48 +0000

    creada estructura básica de los formularios 4, 5, 6

--- src/pages/IC/IcForm5.tsx
+++ src/pages/IC/IcForm5.tsx
@@ -1,6 +1,14 @@
+import { Header } from '../../components/molecules/Header'
+
+interface Options {
+  value: string;
+  label: string;
+}
 
 export const IcForm5 = () => {
   return (
-    <h2>Formulario 5</h2>
+    <div>
+      <Header formCode='(RE-01-LC)' formName='Formato de practicas higiénicas y medidas de protección' href='/icmain' />
+    </div>
   )
 }
--- src/pages/IC/IcForm6.tsx
+++ src/pages/IC/IcForm6.tsx
@@ -1,6 +1,15 @@
+import { Header } from "../../components/molecules/Header"
+
+interface Options {
+  value: string;
+  label: string;
+}
 
 export const IcForm6 = () => {
   return (
-    <h2>Formulario 6</h2>
+    <div>
+      <Header formCode='(RE-08-LC)' formName='Control de aspersiones ambientales.' href='/icmain' />
+      
+    </div>
   )
 }
