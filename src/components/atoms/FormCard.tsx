0f8af04e5ea2f50397cb1296dbd155d24a779110
software.project.zero <software.project.zero@gmail.com>
2025-05-15 19:10:37 +0000

    ajuste formato IcMain y FormCard

--- src/components/atoms/FormCard.tsx
+++ src/components/atoms/FormCard.tsx
@@ -9,9 +9,9 @@ interface FormCardProps {
 
 export const FormCard: React.FC<FormCardProps> = ({ title, description, to }) => {
   return (
-    <NavLink to={to} className="block max-w-sm p-4 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
-      <h5 className="text-md font-bold tracking-tight text-gray-300">{title}</h5>
-      <p className="text-sm text-gray-400">{description}</p>
+    <NavLink to={to} className="block max-w-full p-6 bg-gray-800 border border-gray-200 rounded-lg shadow ">
+      <h5 className="text-lg font-bold tracking-wired text-gray-300">{title}</h5>
+      <p className="text-md text-gray-400">{description}</p>
     </NavLink>
   );
 };
--- src/pages/IC/IcMain.tsx
+++ src/pages/IC/IcMain.tsx
@@ -5,36 +5,36 @@ export const IcMain = () => {
   return (
     <div className=''>
       <Header formName='Inspección de Calidad' formCode='IC' />
-      <section className='grid grid-cols-2 md:grid-cols-2 lg:grid-cols-4 gap-4 m-2 p-2'>
+      <section className='flex flex-col gap-4 m-2 p-2'>
         <FormCard 
-            title='(RE-09-LC)'
-            description='Control de concentración de producto en filtro sanitario'
-            to='/form1'
+          title='(RE-09-LC)'
+          description='Control de concentración de producto en filtro sanitario'
+          to='/form1'
         />
         <FormCard 
-            title='(RE-05-LC)'
-            description='Verificación de producto terminado.'
-            to='/form2'
+          title='(RE-05-LC)'
+          description='Verificación de producto terminado.'
+          to='/form2'
         />
         <FormCard 
-            title='(RE-06-LC)'
-            description='Verificación de condiciones ambientales.'
-            to='/form3'
+          title='(RE-06-LC)'
+          description='Verificación de condiciones ambientales.'
+          to='/form3'
         />
         <FormCard 
-            title='(RE-07-LC)'
-            description='Inspección de limpieza y desinfección en planta.'
-            to='/form4'
+          title='(RE-07-LC)'
+          description='Inspección de limpieza y desinfección en planta.'
+          to='/form4'
         />
         <FormCard 
-            title='(RE-01-LC)'
-            description='Formato de practicas higiénicas y medidas de protección.'
-            to='/form5'
+          title='(RE-01-LC)'
+          description='Formato de practicas higiénicas y medidas de protección.'
+          to='/form5'
         />
         <FormCard 
-            title='(RE-08-LC)'
-            description='Control de aspersiones ambientales.'
-            to='/form6'
+          title='(RE-08-LC)'
+          description='Control de aspersiones ambientales.'
+          to='/form6'
         />
       </section>
     </div>
